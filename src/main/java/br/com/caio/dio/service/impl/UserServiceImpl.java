1fe69d422be75e9ab06c787f77471067f193981d
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2024-07-05 21:17:35 -0300

    implementando camada de negocio

---
+++ src/main/java/br/com/caio/dio/service/impl/UserServiceImpl.java
@@ -0,0 +1,31 @@
+package br.com.caio.dio.service.impl;
+
+import br.com.caio.dio.domain.model.User;
+import br.com.caio.dio.domain.repository.UserRepository;
+import br.com.caio.dio.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public User create(User userToCreate) {
+        if (userRepository.existsByAccountNumber(userToCreate.getAccount().getNumber())) {
+            throw new IllegalArgumentException("This Account number already existis.");
+        }
+        return userRepository.save(userToCreate);
+    }
+}
